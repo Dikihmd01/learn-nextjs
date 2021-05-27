b077cd9efdae8346ef252c29553e73c543ecf987
Diki Hamdani <dikihamdani.id@gmail.com>
2021-05-27 16:53:01 +0700

    next js blog init

---
+++ pages/posts/first-post.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '../../components/layout'
+
+export default function FirstPost() {
+    return (
+        <>
+            <Layout>
+                <Head>
+                    <title>First Post</title>
+                </Head>
+
+                <h1>First Post</h1>
+                <ul>
+                    <li>
+                        <Link href="/">
+                            <a>Back to home</a>
+                        </Link>
+                    </li>
+                </ul>
+            </Layout>
+        </>
+    )
+}
