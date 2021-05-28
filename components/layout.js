21cfcb98b096e258c4a8a22db38bb46a22f09c04
Dikihmd01 <dikihamdani.id@gmail.com>
2021-05-28 16:32:32 +0700

    blog day 2

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,103 @@
+import Head from 'next/head'
+import Image from 'next/image'
 import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
 
-export default function Layout({ children }) {
-    return <div className={styles.container}>{children}</div>
+const name = 'Diki Hamdani'
+export const siteTitle = 'Belajar Next.JS'
+
+export default function Layout({ children, home }) {
+    return (
+        <div className={styles.container}>
+
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <meta
+                name="description"
+                content="Learn how to build a personal website using Next.js"
+                />
+                
+                <meta
+                property="og:image"
+                content={`https://og-image.vercel.app/${encodeURI(
+                    siteTitle
+                )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+                />
+
+                <meta name="og:title" content={siteTitle} />
+                <meta name="twitter:card" content="summary_large_image" />
+            </Head>
+
+            <header className={styles.header}>
+                {home ? (
+                <>
+                    {/* <Image
+                    priority
+                    src="/images/profile.png"
+                    className={utilStyles.borderCircle}
+                    height={200}
+                    width={180}
+                    alt={name}
+                    /> */}
+
+                    <div className={styles.page}>
+                        <nav className={`${styles["page__menu"]} $ {styles["page__custom_settings"]} ${styles["menu"]}`}>
+                            <ul className={`${styles["menu__list"]} ${styles["r-list"]}`}>
+                                <li className={`${styles["menu_group"]}`}>
+                                    <a href="#0" className={`${styles["menu__link"]} ${styles["r-link"]} ${styles["text-underlined"]}`}>Option #1</a>
+                                </li>
+
+                                <li className={`${styles["menu_group"]}`}>
+                                    <a href="#0" className={`${styles["menu__link"]} ${styles["r-link"]} ${styles["text-underlined"]}`}>Option #2</a>
+                                </li>
+
+                                <li className={`${styles["menu_group"]}`}>
+                                    <a href="#0" className={`${styles["menu__link"]} ${styles["r-link"]} ${styles["text-underlined"]}`}>Option #3</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div><br/><br/><br/>
+
+
+                    {/* <h1 className={utilStyles.heading2Xl}>{name}</h1> */}
+                </>
+                ) : (
+                <>
+                    <div className={styles.page}>
+                        <nav className={`${styles["page__menu"]} $ {styles["page__custom_settings"]} ${styles["menu"]}`}>
+                            <ul className={`${styles["menu__list"]} ${styles["r-list"]}`}>
+                                <li className={`${styles["menu_group"]}`}>
+                                    <a href="#0" className={`${styles["menu__link"]} ${styles["r-link"]} ${styles["text-underlined"]}`}>Option #1</a>
+                                </li>
+
+                                <li className={`${styles["menu_group"]}`}>
+                                    <a href="#0" className={`${styles["menu__link"]} ${styles["r-link"]} ${styles["text-underlined"]}`}>Option #2</a>
+                                </li>
+
+                                <li className={`${styles["menu_group"]}`}>
+                                    <a href="#0" className={`${styles["menu__link"]} ${styles["r-link"]} ${styles["text-underlined"]}`}>Option #3</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div><br/><br/><br/>
+                </>
+                )}
+            </header>
+
+            <main className={styles.main}>{children}</main>
+
+            {!home && (
+                <div className={styles.backToHome}>
+                <Link href="/">
+                    <a>← Back to home</a>
+                </Link>
+                </div>
+            )}
+
+            <div className={styles.footer}>This is a footer.
+                This stays at the bottom of the page.
+            </div>
+        </div>        
+    )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,47 @@
-import Image from 'next/image';
+import utilStyles from '../styles/utils.module.css'
 import Head from 'next/head'
 import Link from 'next/link'
-import Layout from '../components/layout';
+import Layout, {siteTitle} from '../components/layout';
+import {getSortedPostsData} from '../lib/posts'
 
-export const Profile = () => (
-  <>
-    <Layout>
+export default function Home({allPostsData}) {
+  return (
+    <Layout home>
       <Head>
-        <title>Homepage</title>
-      </Head>
+          <title>{siteTitle}</title>
+       </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hi, I am Diki Hamdani. I live in Tasikmalaya, Indonesia. I currently learn Next.Js.</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
 
-      <Image src="/images/profile.jpg" alt="my picture" width={50} height={50} />
-
-      <h3>Postingan terbaru</h3>
-      <ul>
-        <li>
-          <Link href="/posts/first-post">
-            <a>First post</a>
-          </Link>
-        </li>
-      </ul>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title, description }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <p>{description}</p>
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
     </Layout>
-  </>
-);
-export default Profile;
+  )
+} 
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
