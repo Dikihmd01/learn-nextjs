1780629f6ad1ee9afcf962628cc7b06b5ceb1ad7
Dikihmd01 <dikihamdani.id@gmail.com>
2021-05-29 16:20:26 +0700

    change id.js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,8 @@
 import Layout from '../../components/layout'
 import {getAllPostIds, getPostData} from '../../lib/posts'
 import Head from 'next/head'
+// import utilStyles from '../../styles/utils.module.css'
+import Date from '../../components/date'
 
 export default function Post({postData}) {
     return (
@@ -9,13 +11,20 @@ export default function Post({postData}) {
                 <title>{postData.title}</title>
             </Head>
 
-            {postData.title} 
-            <br />
-            {postData.id}
-            <br />
-            {postData.date}
+            <h3 className="text-5xl font-bold font-sans mt-3">{postData.title}</h3> 
+            {/* <br /> */}
+            {/* {postData.id} */}
+            {/* <br /> */}
+            <p className="italic text-sm mt-2 ml-2">
+                <Date dateString={postData.date}/>
+            </p>
             <br/>
+
             <div dangerouslySetInnerHTML={{__html: postData.contentHtml}} />
+
+            <div class="font-sans">
+				<p class="text-base md:text-sm text-green-500 font-bold py-6">&lt; <a href="/" class="text-base md:text-sm text-green-500 font-bold no-underline hover:underline">Back to Home</a></p>
+			</div>
         </Layout>
     )
 }
